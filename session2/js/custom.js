4c5a686272e8edb9e98228edfdeda62df027816f
marwaMRadwan <marwaradwan872@hotmail.com>
2022-04-04 11:24:22 +0200

    s2

--- session2/js/custom.js
+++ session2/js/custom.js
@@ -1,5 +1,6 @@
 const addUser = document.querySelector("#addUser")
 const userHeads = ["name", "age", "job", "country"]
+const dataWrapper= document.querySelector("#dataWrapper")
 // read from storage
 const readFromStorage = (storageKey="users") =>{
     let data
@@ -16,24 +17,39 @@ const readFromStorage = (storageKey="users") =>{
 const writeToStorage = (data=[], storageKey="users")=>{
     localStorage.setItem(storageKey, JSON.stringify(data))
 }
-
-addUser.addEventListener("submit", function(e){
-    e.preventDefault()
-    // console.log(this.elements.name.value)
-    let user = { id:Date.now() }
-    userHeads.forEach( head => user[head] = this.elements[head].value)
-    const allUsers = readFromStorage()
-    allUsers.push(user)
-    writeToStorage(allUsers)
-    this.reset()
-    window.location.href = "index.html";
-})
-
-
-
-
 // create 
+if(addUser){
+    addUser.addEventListener("submit", function(e){
+        e.preventDefault()
+        // console.log(this.elements.name.value)
+        let user = { id:Date.now() }
+        userHeads.forEach( head => user[head] = this.elements[head].value)
+        const allUsers = readFromStorage()
+        allUsers.push(user)
+        writeToStorage(allUsers)
+        this.reset()
+        window.location.href = "index.html";
+    })    
+}
 // read 
+const createMyOwnElement = (parent, ele, text=null)=>{
+    const myElement = document.createElement(ele)
+    parent.appendChild(myElement)
+    if(text) myElement.textContent = text
+    return myElement
+}
+if(dataWrapper){
+    const allUsers = readFromStorage()
+    allUsers.forEach((user,index)=>{
+        const tr = createMyOwnElement(dataWrapper, "tr")
+        createMyOwnElement(tr, "td", index+1)
+        createMyOwnElement(tr, "td", user.id)
+        userHeads.forEach(head=> createMyOwnElement(tr, "td", user[head]))
+    })
+}
+
+
+
 // update 
 // delete 
 // show single
