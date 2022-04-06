b018a167098b5c9e746439c2add045def0c79dc0
marwaMRadwan <marwaradwan872@hotmail.com>
2022-04-06 09:55:49 +0200

    s4

---
+++ session4/controllers/dealWithJson.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+const chalk = require("chalk")
+const writeDataToJSON = (fileName, data) => {
+    try{
+        fs.writeFileSync(`db/${fileName}`, JSON.stringify(data))
+        console.log(chalk.green(`data added to file ${fileName}`));
+    }
+    catch(e){
+        console.log(chalk.red("invalid data"))
+    }
+}
+
+const readDataFromJSON = (fileName) => {
+    let data
+    try{
+        data = JSON.parse(fs.readFileSync(`db/${fileName}`))
+        if(!Array.isArray(data)) throw new Error()
+        console.log(chalk.green('data fetched'));
+    }
+    catch(e){
+        data = []
+        console.log(chalk.red("error in reading data"))
+    }
+    return data
+}
+
+module.exports = {
+    writeDataToJSON,
+    readDataFromJSON
+}
---
+++ session4/index.js
@@ -0,0 +1,11 @@
+const deal = require('./controllers/dealWithJson')
+// const d = deal.readDataFromJSON("tasks.json")
+// console.log(d);
+
+// data = [
+//     {id:1,title:"a", content:"aa"},
+//     {id:2,title:"b", content:"bb"},
+//     {id:3,title:"c", content:"cc"},
+//     {id:4,title:"d", content:"dd"},
+// ]
+// deal.writeDataToJSON("tasks.json", data)
