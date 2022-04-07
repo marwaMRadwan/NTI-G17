f060f2c2124a55dab18f0de7d743e6a2e8f29c9e
marwaMRadwan <marwaradwan872@hotmail.com>
2022-04-07 11:32:29 +0200

    s5

--- session5/src/app.js
+++ session5/src/app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const path = require("path")
+const hbs = require("hbs")
+
 const app = express()
+
 const staticDir = path.join(__dirname, '../frontend/public')
+const viewsDir = path.join(__dirname, '../frontend/views')
+const layoutsDir = path.join(__dirname, '../frontend/layouts')
+
 //use static directory
 app.use(express.static(staticDir))
 app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(layoutsDir)
+
 app.get('/', (req,res)=>{
     res.render('home')
 })
