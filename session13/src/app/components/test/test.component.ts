5b10cc3928a4ac0a895893a4062c6ba84dbf03f9
NouranFayez <nouran_ahmed269@hotmail.com>
2022-04-21 14:59:07 +0200

    s13

--- session13/src/app/components/test/test.component.ts
+++ session13/src/app/components/test/test.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  flag=true
   postData:any[] = [
     {
     "userId": 1,
