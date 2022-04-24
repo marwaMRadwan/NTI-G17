800425404def90aad7b27f2625192a8cc39c4ed1
marwaMRadwan <marwaradwan872@hotmail.com>
2022-04-24 14:13:50 +0200

    cors

--- session11/routes/post.api.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const Post = require("../app/controller/post.controller")
-const auth = require("../app/middleware/auth")
-router.post('/add', auth, Post.addPost)
-router.get('/myPosts', auth, Post.myPost)
-
-module.exports = router
